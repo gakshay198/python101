1ab3b3d9761bb8e6dc85f0e22bab45adedd41d20
gakshay198 <gakshay198@gmail.com>
2018-03-31 00:34:44 +0530

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# A comment so you can read your program later
+# Anything after # is ignored by Python.
+print "I could have a code like this." # comment will be ignored.
+
+# Comments are useful to disable some chunk of code "temporarily".
+# this won't run
+
+print "This will run."
---
+++ math.py
@@ -0,0 +1,23 @@
+print "I will now count my chickens:"   # Prints just a simple line.
+
+print "Hens", 25 + 30 / 6               # Performs division first then addition.Then prints the result as 30.
+print "roosters", 100 - 25 * 3 % 4      # Same as above.
+
+print "Now I'll count the eggs:"        # print the line
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6  # Perform operations first then print the result.
+
+print "Is this true that 3 + 2 < 5 - 7?"# Print the line as the numbers are between quotes.
+
+print 3 + 2 <  5 - 7                    # do addition and sustraction first then comapare. Give boolean as output. False in this case.
+
+print "what is  3 + 2?", 3 + 2          # Print the line. Perform operation after , and print the result.
+print "What is 5 - 7?", 5 -7            # same as above.
+
+print "Oh, that's why it is false."     # print the line.
+
+print "How about some more?"            # print the line.
+
+print "Is it greater?", 5 > -2          # print the line and give boolean output.
+print "Is it greater or equal?", 5 >= -2  # same as above.
+print "Is it less or equal?", 5 <= -2   # same as above.
