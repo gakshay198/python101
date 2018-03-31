c8cec7f1f5bd237c591d61b51f0066de866541e1
Akshay <gakshay198@gmail.com>
2018-03-31 11:20:35 +0530

    Working with variables and some calculations using variables.

--- ex4.py
+++ ex4.py
@@ -1,15 +1,23 @@
+# Declare variables and asign values to them
 cars = 100
-space = 4.0
+# float can also be asigned as a value
+space = 4
+# 30 drivers available today.
 drivers = 30
+# There are 90 passengers.
 passengers = 90
+# Available cars minus drivers will give the cars not driven.
 not_driven_cars = cars - drivers
+# Numbers of driven cars is same as number of drivers.
 drivencars = drivers
-carpool_capacity = cars * space
+# The below line will give float output since one of the operand is float.
+carpool_capacity = drivencars * space
+# Total passengers divided by driven cars is average passengers per car.
 average_passangers = passengers / drivencars
 
 print "There are", cars, "available"
 print "But there are only", drivers, "availble"
 print "There willl be", not_driven_cars, "empty cars today"
-print "we can transport", carpool_capacity, "people today"
+print "we can transport", carpool_capacity, "people"
 print "we have", passengers, "passengers to carpoool today"
 print "we need to put about", average_passangers, "in each car today"
---
+++ varcalc.py
@@ -0,0 +1,18 @@
+i = 23
+j = 12
+
+# Overriding value of x multiple times and giving outputs
+x = i + j
+print "Addition : 23 + 12 is", x
+x = i - j
+print "Substraction: 23 - 12 is", x
+x = i * j
+print "Multiplication : 23 * 12 is", x
+x = i / j
+y = i % j
+print "Division : 23 / 12 is", x, "and remainder : ", y
+
+# To output dividion in floating point
+i = 23.0
+x = i / j
+print "Division : 23 / 12", x
