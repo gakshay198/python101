9a4f7b8dc70809ee8a9cf54f1bb7a1b407afc94d
Akshay <gakshay198@gmail.com>
2018-04-01 12:46:14 +0530

    Switching to Python 3.6 from Python 2.7. STarting all over again....

--- Variables/study.py
+++
@@ -1,7 +0,0 @@
-cm = 26
-inches = cm / 2.54
-pounds = 879
-kilos = pounds / 2.2046226218
-
-print "%d cm in inches is : %.2f inches" % (cm, inches)
-print "%d pounds in kilos is : %.3f kilos" % (pounds, kilos)
